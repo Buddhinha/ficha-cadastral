f04f5732e53ae653cf38faca3a5449179cb66073
Arthur <153740966+Buddhinha@users.noreply.github.com>
2024-11-23 15:48:39 -0300

    Update calc.php

--- calc.php
+++ calc.php
@@ -1,68 +1,50 @@
-<!DOCTYPE html>
-<html lang="pt-br">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Explorando Variáveis em PHP</title>
-    <style>
-        body {
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            height: 100vh;
-            margin: 0;
-        }
-
-        .container {
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            height: 100%;
-        }
-
-        .card {
-            background: #fff;
-            border-radius: 8px;
-            box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
-            padding: 20px;
-            max-width: 400px;
-            text-align: center;
-        }
-
-        h1 {
-            color: #333;
-        }
-
-        p {
-            color: #666;
-            font-size: 1.1em;
-        }
-
-        strong {
-            color: #000;
-        }
-    </style>
-</head>
 <?php 
-    require"info.php";
-    require"calc.php";
-?>
+require"info.php";
 
-<body>
-    <div class="container">
-        <div class="card">
-            <h1>Ficha Cadastral</h1>
-            <p>Nome: <strong><?= $nome ?></strong></p>
-            <p>Idade: <strong><?= $idade ?></strong></p>
-            <p>Sexo: <strong><?= $sexo ?></strong></p>
-            <p>Salário Mensal: <strong><?= numerosBrasileiros($salario)?></strong></p>
-            <p>Salário Anual: <strong><?= salarioAnual($salario)?></strong></p>
-            <p>Status de Emprego: <strong><?= $empregado ?></strong></p>
-            <p>Anos para A aposentadoria<strong> <?=anosParaAposentadoria(15, $sexo = true)?></strong></p>
-            <p>Habilidades: <strong><?= $habilidade ?></strong></p>
-        </div>
-    </div>
-</body>
+$empregado = $empregado == true ? "empregado" : "não empregado";
 
-</html>
+
+
+
+/**
+ * calcula os anos faltantes para aposentadoria
+ * @param int $idade idade do cidadão
+ * @param bool true para masculo e false para mulher
+ * @return int retorna os anos até a aposentadoria
+ */
+function anosParaAposentadoria(int $idade,bool $sexo = true){
+    $anosParaAposentarMasculino = 65;
+    $anosParaAposentarFeminino  = 62;
+ 
+    $anosTotaisParaAposentar = $sexo == true ? $anosParaAposentarMasculino - $idade : $anosParaAposentarFeminino - $idade;
+
+    return $anosTotaisParaAposentar;
+
+}
+
+
+
+
+
+$habilidades = ["php", "javascript", "html", "css"];
+$habilidade = implode(",", $habilidades);
+
+/**
+ *faz o calculo do salario anual junto com o decimo terceiro.
+ * @param int $salario aqui é o salario mensal
+ * @return float 
+ */ 
+function salarioAnual (float $salario):string{
+    //$numeroFormatado = number_format($numero, 2,",",".");
+    $decimoTerceiro =$salario /3;
+    $salarioAnual = ($salario*12)+ $decimoTerceiro;
+    return numerosBrasileiros($salarioAnual);
+    
+}
+
+function numerosBrasileiros(float $numero):string{
+    $numero = number_format($numero, 2,",",".");
+    return $numero;
+}
+
+
